cd751d647eb8c39e0e349918d88da85c9e4fcf3a
Mara <marastodulka@gmail.com>
2025-07-29 17:57:10 +0200

    started working on movement and put cherry spawning logic in place

---
+++ levels.lua
@@ -0,0 +1,1 @@
+--this file is gonna contain the game levels
--- main.lua
+++ main.lua
@@ -4,8 +4,10 @@ local timer = 0
 local interval = 0.3
 local dir = "right" -- Initial direction
 local Packman = {
-    {x = 10, y = 10}}
+    {x = 12, y = 9} -- Initial position of Packman
+    }
 local cherryspwn_delay = 0
+local cherry = nil -- Variable to hold the cherry position
 local gameover = false
 local font_size
 local song
@@ -17,35 +19,46 @@ local lives = 3
 local points = 0
 local invicibility = false
 local points = 0
-
+local cherryspwn_delay = 0
 
 function love.load()
     love.window.setTitle("Packman - temu edition")
     love.window.setMode(800, 600, { resizable = false, vsync = true })
     font_size = love.graphics.newFont(20)
-    packman_image = love.graphics.newImage("packman.aseprite")
-    point_image = love.graphics.newImage("point.aseprite")
-    wall_image = love.graphics.newImage("wall.aseprite")
-    cherry_image = love.graphics.newImage("cherry.aseprite")
-    bigPoint_image = love.graphics.newImage("bigPoint.aseprite")
-    blinky_image = love.graphics.newImage("blinky.aseprite")
-    inky_image = love.graphics.newImage("inky.aseprite")
-    pinky_image = love.graphics.newImage("pinky.aseprite")
-    clyde_image = love.graphics.newImage("clyde.aseprite")
-    cherrySound = love.audio.newSource("", "static")
+    packman_image = love.graphics.newImage("packman.png")
+    point_image = love.graphics.newImage("point.png")
+    wall_image = love.graphics.newImage("wall.png")
+    cherry_image = love.graphics.newImage("cherry.png")
+    bigPoint_image = love.graphics.newImage("bigPoint.png")
+    blinky_image = love.graphics.newImage("blinky.png")
+    inky_image = love.graphics.newImage("inky.png")
+    pinky_image = love.graphics.newImage("pinky.png")
+    clyde_image = love.graphics.newImage("clyde.png")
+    --[[cherrySound = love.audio.newSource("", "static")
     song = love.audio.newSource("", "stream")
     death = love.audio.newSource("", "stream")
+    ]]
 end
-
 
 function love.update(dt)
     if not gameover then --checks if the game is over
-        timer = timer + dt-- basic timer for the snake movement and ingame stuff
+        timer = timer + dt
+        cherryspwn_delay = cherryspwn_delay + dt -- timer for spawning cherries
         if timer >= interval then
             timer = 0
+            local player = Packman[1]
+            local newX, newY = player.x, player.y
+            if dir == "right" then newX = newX + 1 end
+            if dir == "left" then newX = newX - 1 end
+            if dir == "up" then newY = newY - 1 end
+            if dir == "down" then newY = newY + 1 end
+        if cherryspwn_delay >= 5 then -- spawn a cherry every 5 seconds
+            cherryspwn_delay = 0
+            cherry = {x = math.random(0, 24), y = math.random(0, 18)} -- random position for the cherry
+            --cherrySound:play() -- play sound when cherry spawns
+        end
         end
     end
-    
 end
 
 function love.draw()
@@ -54,5 +67,27 @@ function love.draw()
     local pointsText = "Points: " .. tostring(points)
     local textWidth = font_size:getWidth(pointsText)
     love.graphics.print(pointsText, love.graphics.getWidth() - textWidth - 10, 10)
-    
+    local player = Packman[1]
+    love.graphics.draw(packman_image, player.x * gridSize, player.y * gridSize)
+    if cherry then
+        love.graphics.draw(cherry_image, cherry.x * gridSize, cherry.y * gridSize)
+    end
+
+    if gameover then -- if the game is over it draws the game over screen
+        love.graphics.setFont(font_size)
+        love.graphics.setColor(1, 0, 0, 1) --red
+        love.graphics.printf("YOU LOST, press r to restart the game", 0, love.graphics.getHeight() / 2 - 20, love.graphics.getWidth(), "center")
+    end
+end
+
+--simple key controls
+function love.keypressed(key)
+    if key == "d" and dir ~= "left" then dir = "right" end
+    if key == "a" and dir ~= "right" then dir = "left" end
+    if key == "w" and dir ~= "down" then dir = "up" end
+    if key == "s" and dir ~= "up" then dir = "down" end
+    if key == "r" and gameover then
+        love.event.quit("restart") -- Restart the game
+        gameover = false
+    end
 end
